5de7b996c45f1525d0bdf0bc33852343ece2fd80
Dhimas Prajaya <dhimasprajaya@gmail.com>
2020-05-21 04:48:56 +0700

    displaying user image

--- lib/widgets/chat/new_message.dart
+++ lib/widgets/chat/new_message.dart
@@ -20,6 +20,7 @@ class _NewMessageState extends State<NewMessage> {
       'createdAt': Timestamp.now(),
       'userId': user.uid,
       'username': userData['username'],
+      'userImage': userData['image_url'],
     });
 
     _controller.clear();
