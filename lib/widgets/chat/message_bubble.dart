1bec3f17353df2b7fee58e4a1eb83863feb95a0b
Dhimas Prajaya <dhimasprajaya@gmail.com>
2020-05-21 01:03:47 +0700

    add basic bubble text

---
+++ lib/widgets/chat/message_bubble.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class MessageBubble extends StatelessWidget {
+  MessageBubble(this.message);
+
+  final String message;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Container(
+          decoration: BoxDecoration(
+            color: Theme.of(context).accentColor,
+            borderRadius: BorderRadius.circular(12),
+          ),
+          width: 240,
+          padding: EdgeInsets.symmetric(
+            vertical: 10,
+            horizontal: 16,
+          ),
+          margin: EdgeInsets.symmetric(
+            vertical: 4,
+            horizontal: 8,
+          ),
+          child: Text(
+            message,
+            style: TextStyle(
+              color: Theme.of(context).accentTextTheme.headline1.color,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
