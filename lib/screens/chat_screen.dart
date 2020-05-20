1ce387b60a9859132b905fc3d9e9656dd659989d
Dhimas Prajaya <dhimasprajaya@gmail.com>
2020-05-21 00:52:42 +0700

    send chat and ordering

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -3,6 +3,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 import '../widgets/chat/messages.dart';
+import '../widgets/chat/new_message.dart';
 
 class ChatScreen extends StatelessWidget {
   @override
@@ -46,16 +47,9 @@ class ChatScreen extends StatelessWidget {
             Expanded(
               child: Messages(),
             ),
+            NewMessage(),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          Firestore.instance
-              .collection('chats/KFeoUyhDSXVndsSPRLUs/messages')
-              .add({'text': 'This was added by clicking the button!'});
-        },
       ),
     );
   }
