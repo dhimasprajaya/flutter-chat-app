d5cb2d8019d478f02e8bbee6bafd9c938e36ca2c
Dhimas Prajaya <dhimasprajaya@gmail.com>
2020-05-20 22:50:56 +0700

    add basic auth screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import './screens/auth_screen.dart';
 import './screens/chat_screen.dart';
 
 void main() => runApp(MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: ChatScreen(),
+      home: AuthScreen(),
     );
   }
 }
