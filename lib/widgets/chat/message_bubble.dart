3009a159446d187f49353e5557bdd0d77391dea7
Dhimas Prajaya <dhimasprajaya@gmail.com>
2020-05-21 01:32:38 +0700

    improve bubble chat

--- lib/widgets/chat/message_bubble.dart
+++ lib/widgets/chat/message_bubble.dart
@@ -1,18 +1,30 @@
 import 'package:flutter/material.dart';
 
 class MessageBubble extends StatelessWidget {
-  MessageBubble(this.message);
+  MessageBubble(
+    this.message,
+    this.isMe,
+    {this.key}
+  );
 
+  final Key key;
   final String message;
+  final bool isMe;
 
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
       children: <Widget>[
         Container(
           decoration: BoxDecoration(
-            color: Theme.of(context).accentColor,
-            borderRadius: BorderRadius.circular(12),
+            color: isMe ? Colors.grey[300] : Theme.of(context).accentColor,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(12),
+              topRight: Radius.circular(12),
+              bottomLeft: !isMe ? Radius.circular(0) : Radius.circular(12),
+              bottomRight: isMe ? Radius.circular(0) : Radius.circular(12),
+            ),
           ),
           width: 240,
           padding: EdgeInsets.symmetric(
@@ -26,7 +38,9 @@ class MessageBubble extends StatelessWidget {
           child: Text(
             message,
             style: TextStyle(
-              color: Theme.of(context).accentTextTheme.headline1.color,
+              color: isMe
+                  ? Colors.black
+                  : Theme.of(context).accentTextTheme.headline1.color,
             ),
           ),
         ),
