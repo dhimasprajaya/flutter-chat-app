1f6f6c18654738dd8dd9d9efed7e9f37188784e9
Dhimas Prajaya <dhimasprajaya@gmail.com>
2020-05-21 07:19:21 +0700

    setting firestore triger

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,4 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +6,11 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+exports.myFunction = functions.firestore
+  .document("chat/{message}")
+  .onCreate((snap, context) => {
+    const newValue = snap.data();
+    console.log(newValue);
+    return;
+  });
